4757ceb4b90f8bf52e3e13b732f8dd002416c9af
Dreanor <mikeundead@googlemail.com>
2021-03-12 23:59:48 +0100

    init

---
+++ loadscreen/__resource.lua
@@ -0,0 +1,12 @@
+description 'Custom Loadscreen'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+    'index.html',
+    'intro.mp4',
+    'styles.css',
+    'music.mp3'
+}
+
+loadscreen 'index.html'
